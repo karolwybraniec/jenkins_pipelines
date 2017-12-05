fde4b3c664eb209d80b4df926d5cec696db85f55
Karol Wybraniec <karolwybraniec@outlook.com>
2017-12-05 20:32:10 +0100

    2nd trial

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-def run():
+import sys
+
+
+def test():
     print("[Step] Test")
     assert 1 == 1
 
 
+def parse():
+    print("[Step] Parse")
+    assert 1 == 1
+
+
+def deploy():
+    print("[Step] Deploy")
+    assert 1 == 1
+
+
 if __name__ == '__main__':
-    run()
+    if sys.argv[1] == 'test':
+        test()
+    elif sys.argv[1] == 'parse':
+        parse()
+    elif sys.argv[1] == 'deploy':
+        deploy()
